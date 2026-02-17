51405fdcc56904069e88ff877cc52dc2c16b14e5
Juan Esteban Cardona <juan.cardonacal@campusucc.edu.co>
2026-02-17 03:38:39 +0000

    desarrollo de los ejercicios a conciencia.

--- Momento_1/Tareas/Taller_ejercicios/Ejercicio4.java
+++ Momento_1/Tareas/Taller_ejercicios/Ejercicio4.java
@@ -3,3 +3,27 @@ import java.util.*;
 public class Ejercicio4 {
 
     public static void main(String[] args) {
+
+        int[] edades1 = { 18, 19, 22, 20, 21};
+        int[] edades2 = { 17, 20, 19, 18, 25};
+
+        int mayor1 = definirMayor(edades1);
+        int mayor2 = definirMayor(edades2);
+
+        System.out.println("La mayor edad del primer arreglo es: " + mayor1);                                                                           
+        System.out.println("La mayor edad del segundo arreglo es: " + mayor2);
+
+    }
+
+    public static int definirMayor(int[] array) {
+            int mayor = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] > mayor) {
+                    mayor = array[i];
+                }
+            }
+            return mayor;
+        }
+
+    
+}
