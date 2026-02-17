5453f96ffa798f8f5d7d1a97071b8f176338674d
Juan Esteban Cardona <juan.cardonacal@campusucc.edu.co>
2026-02-17 02:12:08 +0000

    Entendiendo nuevamente el uso de los metodos.

--- Momento_1/Tareas/Taller_ejercicios/Ejercicio4.java
+++ Momento_1/Tareas/Taller_ejercicios/Ejercicio4.java
@@ -2,9 +2,4 @@ import java.util.*;
 
 public class Ejercicio4 {
 
-    String[] nombres = {"Carlos", "Ana", "Juan", "Jhonson", "Marcos"};
-    int[] edades = {17, 19, 22, 25, 20};
-
-    for (int i = 0; i < nombres.length; i++) {
-        if
-}
+    public static void main(String[] args) {
